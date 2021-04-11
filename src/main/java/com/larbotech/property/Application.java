454bf43b2e6bd67a9230f9d205b0fb03b0958f0a
Mahamadou TOURE <touremohamed@@gmail.com>
2021-04-11 13:16:30 +0200

    add asciitable dependency to display report on console

--- src/main/java/com/larbotech/property/Application.java
+++ src/main/java/com/larbotech/property/Application.java
@@ -12,10 +12,10 @@ public class Application {
   public static void main(String[] args) {
     List<Project> projects = new ArrayList<>();
 
-    projects.add(new Project("toure", "etoe", 90.0, 2300L, 1445));
-    projects.add(new Project("larbo", "asset2", 99.0, 200L, 145));
-    projects.add(new Project("berah", "fc", 100.0, 2800L, 2945));
-    projects.add(new Project("maig", "gao", 80.2, 50000L, 445));
+    projects.add(new Project("Larbou", "etoe", 90.0, 2300L, 1445));
+    projects.add(new Project("Larbou", "asset2", 99.0, 200L, 145));
+    projects.add(new Project("Larbou", "fc", 100.0, 2800L, 2945));
+    projects.add(new Project("Larbou", "gao", 80.2, 50000L, 445));
 
     Show.showResultForTeam(new Team("Larbou", projects));
 
--- src/main/java/com/larbotech/property/Show.java
+++ src/main/java/com/larbotech/property/Show.java
@@ -1,23 +1,42 @@
 package com.larbotech.property;
 
+import de.vandermeer.asciitable.AsciiTable;
+
 public class Show {
 
     public static void showResultForTeam(Team team) {
         // Print the list objects in tabular format.
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.printf("%10s %10s %30s %20s %15s", "Equipe", "Asset", "Coverage", "Locs", "Debt");
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        for (Project project : team.getProjects()) {
-            System.out.format("%10s %10s %30f %20d %15s", project.getTeam(), project.getName(), project.getCoverage(),
-                    project.getLocs(), project.readableDebt());
-            System.out.println();
-        }
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.format("%10s %10s %30f %20d %15s", "", "Total", team.getCoverage(), team.getLocs(),
-                team.readableDebt());
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
+
+        AsciiTable at = new AsciiTable();
+        at.addRule();
+        at.addRow("Equipe", "Asset", "Coverage", "Locs", "Debt");
+
+        team.getProjects().forEach(project -> {
+            at.addRule();
+            at.addRow(project.getTeam(), project.getName(), project.getCoverage(), project.getLocs(),
+                    project.readableDebt());
+        });
+
+        at.addRule();
+        at.addRow("Total", team.getName(), team.getCoverage(), team.getLocs(), team.readableDebt());
+        at.addRule();
+        System.out.println(at.render());
+
+    }
+
+    public static void displayReport(Teams teams) {
+        AsciiTable at = new AsciiTable();
+        at.addRule();
+        at.addRow("Equipe", "Coverage", "Locs", "Debt");
+
+        teams.getTeams().forEach(team -> {
+            at.addRule();
+            at.addRow(team.getName(), team.getCoverage(), team.getLocs(), team.readableDebt());
+        });
+
+        at.addRow("Total", teams.getName(), teams.getCoverage(), teams.getLocs(), teams.readableDebt());
+        at.addRule();
+        System.out.println(at.render());
 
     }
 
