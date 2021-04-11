0956839de5c4342157e362c4edc732f763b66f10
Mahamadou TOURE <touremohameda@gmail.com>
2021-04-11 20:44:37 +0200

    Update Project.java

--- src/main/java/com/larbotech/property/Project.java
+++ src/main/java/com/larbotech/property/Project.java
@@ -1,4 +1,4 @@
-package com.larbotech.property;
+package com.bnpp.zephyr.tools.sonar.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,24 +11,28 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Project {
 
-    private static final Integer NB_MIN_J = 1440;
+    private static final Integer NB_MIN_J = 480; //8 (working hours per day)
     private static final Integer NB_MIN_H = 60;
 
     private String team;
     private String name;
     private Double coverage;
-    private Long locs;
+    private Long lineOfCode;
     private Integer debt;
 
     public String readableDebt() {
         return timeConvert(debt);
     }
 
+    /**
+     * The measure of the technical debt is stored in minutes in the DB. Which is consistent with what you see:
+     * 15750 (minutes) / 60 (minutes per hour) / 8 (working hours per day) = 32.8 days
+     */
     public static String timeConvert(Integer time) {
         if (time < NB_MIN_H)
             return time + "MIN";
-        if (time < NB_MIN_J && time >= NB_MIN_H)
-            return time / 60 % 24 + "H " + time % 60 + "MIN";
-        return time / 24 / 60 + "J " + time / 60 % 24 + "H " + time % 60 + "MIN";
+        if (time < NB_MIN_J)
+            return time / 60 % 8 + "H " + time % 60 + "MIN";
+        return time / 8 / 60 + "J " + time / 60 % 8 + "H " + time % 60 + "MIN";
     }
 }
