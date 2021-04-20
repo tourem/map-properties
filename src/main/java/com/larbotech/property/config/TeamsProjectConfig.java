61c4b875eaffec4a50d35de6820ea8de3b2c6cb1
Mahamadou TOURE <touremohameda@gmail.com>
2021-04-20 14:33:12 +0200

    Update TeamsProjectConfig.java

--- src/main/java/com/larbotech/property/config/TeamsProjectConfig.java
+++ src/main/java/com/larbotech/property/config/TeamsProjectConfig.java
@@ -1,18 +1,19 @@
-package com.larbotech.property.config;
+package com.bnpp.zephyr.tools.sonar.config;
 
-import java.util.Map;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotEmpty;
+import java.util.Map;
+
 @Getter
 @Setter
 @Component
 @ConfigurationProperties(prefix = "teams")
-public class TeamsProjectConfig {
-    private int nb;
+public class ProjectTeamConfig {
+    private String sprint;
     private Map<String, KeyBranch> dna;
     private Map<String, KeyBranch> irma;
     private Map<String, KeyBranch> andy;
@@ -20,6 +21,7 @@ public class TeamsProjectConfig {
     @Getter
     @Setter
     public static class KeyBranch {
+        @NotEmpty
         private String key;
         private String branch;
     }
