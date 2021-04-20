596e122929636817f02a6cb515215b65f5daa2e5
Mahamadou TOURE <touremohameda@gmail.com>
2021-04-20 13:52:57 +0200

    Create Constants.java

---
+++ src/main/java/com/larbotech/property/Constants.java
@@ -0,0 +1,9 @@
+package com.bnpp.zephyr.tools.sonar.utils;
+
+public interface Constants {
+  String SUMMARY = "Récapitulatif";
+  String PREFIX_SPRINT = "sprint-";
+  String TOTAL = "Total";
+  String PERCENTAGE = " %";
+
+}
