1d946f317beafaeed7ea523d08abeaee647cc6cd
Mahamadou TOURE <touremohameda@gmail.com>
2021-04-20 13:52:13 +0200

    Update MeasureProvider.java

--- src/main/java/com/larbotech/property/MeasureProvider.java
+++ src/main/java/com/larbotech/property/MeasureProvider.java
@@ -3,10 +3,13 @@ package com.bnpp.zephyr.tools.sonar.providers;
 
 import com.bnpp.zephyr.tools.sonar.exception.BadSonarQubeRequestException;
 import com.bnpp.zephyr.tools.sonar.exception.SonarQubeException;
+import com.bnpp.zephyr.tools.sonar.model.Branch;
+import com.bnpp.zephyr.tools.sonar.model.BranchTypeEnum;
 import com.bnpp.zephyr.tools.sonar.model.Measure;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.apache.commons.lang3.StringUtils;
 import org.sonarqube.ws.client.GetRequest;
 import org.sonarqube.ws.client.HttpConnector;
 import org.sonarqube.ws.client.WsResponse;
@@ -15,7 +18,11 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.max;
 
 /**
  * Provides issue items
@@ -23,9 +30,8 @@ import java.util.List;
 @Component
 public class MeasureProvider {
 
-    private static final String GET_MEASURES_PATH = "/component?componentKey=%s&metricKeys=coverage,sqale_index,ncloc&branch=%s";
-
-    private static final String SPACE = " ";
+    private static final String GET_MEASURES_PATH = "/measures/component?componentKey=%s&metricKeys=coverage,sqale_index,ncloc&branch=%s";
+    private static final String GET_BRANCHES_PATH = "/project_branches/list?project=%s";
 
     /**
      * Field to search in json to get the component
@@ -35,6 +41,11 @@ public class MeasureProvider {
      * Field to search in json to get measures
      */
     private static final String MEASURES = "measures";
+
+    /**
+     * Field to search in json to get branches
+     */
+    private static final String BRANCHES = "branches";
 
     @Autowired
     private HttpConnector httpConnector;
@@ -49,14 +60,16 @@ public class MeasureProvider {
      * @throws BadSonarQubeRequestException when the server does not understand the request
      * @throws SonarQubeException           When SonarQube server is not callable.
      */
-    public List<Measure> getMeasures(String projectKey, String branch) throws BadSonarQubeRequestException, SonarQubeException {
+    public List<Measure> getMeasures(String projectKey, String branch) throws BadSonarQubeRequestException {
         // send a request to sonarqube server and return th response as a json object
         // if there is an error on server side this method throws an exception
-        final JsonObject jo = request(String.format(GET_MEASURES_PATH,
-                projectKey, branch));
+        String branchToAnalyze = getBranchLastAnalysisDateIfBranchIsEmpty(projectKey, branch);
+
+        final JsonObject jsonObject = request(String.format(GET_MEASURES_PATH,
+                projectKey, branchToAnalyze));
 
         // json element containing measure information
-        final JsonElement measuresJE = jo.get(COMPONENT).getAsJsonObject().get(MEASURES);
+        final JsonElement measuresJE = jsonObject.get(COMPONENT).getAsJsonObject().get(MEASURES);
         // put json in a list of measures
         final Measure[] tmp = (gson.fromJson(measuresJE, Measure[].class));
 
@@ -65,6 +78,35 @@ public class MeasureProvider {
         return new ArrayList<>(Arrays.asList(tmp));
     }
 
+    private String getBranchLastAnalysisDateIfBranchIsEmpty(String projectKey, String branchName) throws BadSonarQubeRequestException{
+        if (StringUtils.isEmpty(branchName)){
+            Branch branch = getBranchLastAnalysisDate(projectKey);
+            if (branch != null) return branch.getName();
+        }
+        return branchName;
+    }
+
+    private List<Branch> getBranches(String projectKey) throws BadSonarQubeRequestException {
+        // send a request to sonarqube server and return th response as a json object
+        // if there is an error on server side this method throws an exception
+        final JsonObject jsonObject = request(String.format(GET_BRANCHES_PATH,
+                projectKey));
+
+        // json element containing measure information
+        final JsonElement branches = jsonObject.getAsJsonObject().get(BRANCHES);
+        // put json in a list of measures
+        final Branch[] tmp = (gson.fromJson(branches, Branch[].class));
+
+        // then add all measure to the results list
+        // return the list
+        return new ArrayList<>(Arrays.asList(tmp));
+    }
+
+    public Branch getBranchLastAnalysisDate(String projectKey) throws BadSonarQubeRequestException {
+        //on ne recupère que les branches LONG
+        List<Branch> branches = getBranches(projectKey).stream().filter(b -> b.getType() == BranchTypeEnum.LONG).collect(Collectors.toList());
+        return max(branches, Comparator.comparing(Branch::getAnalysisDate));
+    }
 
     /**
      * Execute a given request
@@ -72,36 +114,28 @@ public class MeasureProvider {
      * @param path Url for the request, for example /toto/list
      * @return Server's response as a JsonObject
      * @throws BadSonarQubeRequestException if SonarQube Server sent an error
-     * @throws SonarQubeException           When SonarQube server is not callable.
      */
-    private JsonObject request(final String path)
-            throws BadSonarQubeRequestException, SonarQubeException {
-        // do the request to the server and return a string answer
-        final String raw = stringRequest(path);
-
-        // prepare json
-        final JsonElement json;
-
-        // verify that the server response was correct
-        try {
-            json = gson.fromJson(raw, JsonElement.class);
-        } catch (Exception e) {
-            throw new BadSonarQubeRequestException("Server answered: " + raw +
-                    SPACE + e.getMessage());
-        }
-
+    private JsonObject request(final String path) throws BadSonarQubeRequestException {
         // get the json object version
         final JsonObject jsonObject;
-
         try {
-            jsonObject = json.getAsJsonObject();
-        } catch (NullPointerException e) {
-            throw new BadSonarQubeRequestException("Empty server response, reason might be : " +
-                    "server certificate not in JRE/JDK truststore, ...");
-        }
+            // do the request to the server and return a string answer
+            final String raw = stringRequest(path);
 
-        // verify if an error occurred
-        isErrorFree(jsonObject);
+            // prepare json
+            final JsonElement json;
+
+            // verify that the server response was correct
+            json = gson.fromJson(raw, JsonElement.class);
+
+            jsonObject = json.getAsJsonObject();
+
+            // verify if an error occurred
+            thrownIfServerHasSentError(jsonObject);
+
+        } catch (Exception e) {
+            throw new BadSonarQubeRequestException(e.getMessage(), e);
+        }
 
         return jsonObject;
     }
@@ -113,7 +147,7 @@ public class MeasureProvider {
      * @param jsonObject The response from the server
      * @throws BadSonarQubeRequestException thrown if the server do not understand our request
      */
-    private void isErrorFree(final JsonObject jsonObject) throws BadSonarQubeRequestException {
+    private void thrownIfServerHasSentError(final JsonObject jsonObject) throws BadSonarQubeRequestException {
         // we retrieve the exception
         final JsonElement error = jsonObject.get("errors");
         // if there is an error we search the message and throw an exception
